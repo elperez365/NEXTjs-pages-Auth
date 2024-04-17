942d8bc294d35e58d12e0f26b4750a0484f17106
vasco ceti <vasco.ceti@gmail.com>
2024-04-17 18:03:59 +0200

    sign in init

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,5 +1,6 @@
 import { useState, useRef } from "react";
 import classes from "./auth-form.module.css";
+import { signIn } from "next-auth/react";
 
 async function createUser(email, password) {
   const response = await fetch("/api/auth/signup", {
@@ -36,7 +37,12 @@ function AuthForm() {
     // optional: Add validation
 
     if (isLogin) {
-      // log user in
+      const result = await signIn("credentials", {
+        redirect: false,
+        email: enteredEmail,
+        password: enteredPassword,
+      });
+      console.log(result);
     } else {
       try {
         const result = await createUser(enteredEmail, enteredPassword);
